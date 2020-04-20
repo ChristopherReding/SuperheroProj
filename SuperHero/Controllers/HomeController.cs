e79ae612097870c41aa771e4760f8f39d346d40b
Christopher Reding <creding21@gmail.com>
2020-04-20 17:12:03 -0500

    added superhero model and table

--- SuperHero/Controllers/HomeController.cs
+++ SuperHero/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SuperHero.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SuperHero.Controllers
 {
