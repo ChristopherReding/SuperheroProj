077f6590f8ab30a2cdd86fb7816a54e4b25389e9
Christopher Reding <creding21@gmail.com>
2020-04-21 15:12:37 -0500

    details and create methods working

--- SuperHero/Controllers/SuperheroController.cs
+++ SuperHero/Controllers/SuperheroController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SuperHero.Data;
+using SuperHero.Models;
 
 namespace SuperHero.Controllers
 {
@@ -21,14 +22,40 @@ namespace SuperHero.Controllers
             var superheroes = _context.Superheroes.ToList();
             return View(superheroes);
         }
-
-        public ActionResult Display()
+        
+        public ActionResult Details(int id)
         {
+            var hero = _context.Superheroes.Where(m => m.Id == id).SingleOrDefault();
+            return View(hero);
+        }
+        
 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var hero = new Superhero();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Superhero hero)
+        {
+            _context.Superheroes.Add(hero);
+            _context.SaveChanges();
+            return RedirectToAction("index");         
+        }
+        
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
             return View();
         }
 
-        
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+                      
         
     }
 }
